f677186ebeb176cb32fc14148e82f0de1d46d985
MertA School Okul <mert.aysel2200@gmail.com>
2025-05-16 18:41:13 +0300

    Optimize code inspection settings in editor configuration
    This commit adds and adjusts a comprehensive list of inspection severities in the editor configuration file, enhancing code analysis capabilities. It introduces precise rules for warnings, suggestions, errors, and hints, which improve code quality and consistency during development.

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,27 @@
 #include <Arduino.h>
+
+int EnA = 3;
+int EnB = 11;
+
+int In1 = 2;
+// int In2 =
+int In3 = 12;
+// int In4 =
 void setup() {
-// write your initialization code here
+    pinMode(EnA, OUTPUT);
+    pinMode(EnB, OUTPUT);
+    pinMode(In1, INPUT);
+    // pinMode(In2, INPUT);
+    pinMode(In3, INPUT);
+    // pinMode(In4, INPUT); 
+
 }
 
 void loop() {
-// write your code here
+    // PWM Pinleri ile hız kontrolü
+    analogWrite(EnA, 255);
+    digitalWrite(In1, HIGH);
+
+    analogWrite(EnB, 255);
+    digitalWrite(In3, HIGH);
 }
