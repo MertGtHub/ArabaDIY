6a1ace406aca0be118e7dc8746c2392220fd47a0
MertA School Okul <mert.aysel2200@gmail.com>
2025-05-16 19:06:55 +0300

    Add missing pin initializations and improve motor control logic
    Defined missing input pins (In2, In4) and updated motor control functions (ileri, geri) to properly control motor directions. This ensures complete and accurate functionality of motor operation with the given pins.

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,15 @@
 #include <Arduino.h>
 
+// Enable Pinleri
 int EnA = 3;
 int EnB = 11;
 
+// İnput pinleri
 int In1 = 2;
-// int In2 =
+int In2 = 4;
 int In3 = 12;
-// int In4 =
+int In4 = 10;
+
 void setup() {
     pinMode(EnA, OUTPUT); //Enable: A
     pinMode(EnB, OUTPUT); //Enable: B
@@ -16,21 +19,26 @@ void setup() {
     pinMode(In4, INPUT); // Mod: Output olabilir.
 
 }
+// PWM Pinleri ile hız kontrolü
 
 void ileri() {
     analogWrite(EnA, 255);
     digitalWrite(In1, HIGH);
+    digitalWrite(In2, LOW);
 
     analogWrite(EnB, 255);
     digitalWrite(In3, HIGH);
+    digitalWrite(In4, LOW);
 }
 
 void geri() {
     analogWrite(EnA, 255);
     digitalWrite(In1, LOW);
+    digitalWrite(In2, HIGH);
 
     analogWrite(EnB, 255);
     digitalWrite(In3, LOW);
+    digitalWrite(In4, HIGH);
 }
 
 
